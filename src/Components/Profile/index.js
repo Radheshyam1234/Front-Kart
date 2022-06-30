7359ff2ebec1d65b8e1f671404635a30def2f8a3
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-06-30 21:54:50 +0530

    fix issues

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -2,5 +2,4 @@ export { ProfilePage } from "./ProfilePage";
 export { MyProfile } from "./MyProfile";
 export { EditProfile } from "./EditProfile";
 export { Orders } from "./Orders";
-export { Setting } from "./Setting";
 export { Addresses } from "./AddressManagement";
